8f856e76624eea9de857ae0c36473dca924482f0
daniel-kolo <kolodani95@gmail.com>
2020-03-04 10:30:28 +0100

    Initial commit

---
+++ src/main/kotlin/com/kotlin_portfolio_manager/KotlinPortfolioManagerApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin_portfolio_manager
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinPortfolioManagerApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinPortfolioManagerApplication>(*args)
+}
